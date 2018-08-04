9d12629a08467b175b85620da87b5a6819d2f676
kavin <kavin@LAPTOP-P5LSKAKC>
2018-08-04 18:52:15 +0530

    updated

--- src/main/java/Excel/Newlyadded1.java
+++ src/main/java/Excel/Newlyadded1.java
@@ -4,8 +4,8 @@ public class Newlyadded1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("karthick");
-
+		System.out.println("Babu");
+		System.out.println("Babu");
 	}
 
 }
