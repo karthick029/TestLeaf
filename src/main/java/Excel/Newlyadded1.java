d0c68fd0567682baf76ca473118c4403e02728ad
kavin <kavin@LAPTOP-P5LSKAKC>
2018-08-04 19:04:01 +0530

    ok

--- src/main/java/Excel/Newlyadded1.java
+++ src/main/java/Excel/Newlyadded1.java
@@ -4,14 +4,6 @@ public class Newlyadded1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
-		System.out.println("Babu");
-		System.out.println("Babu");
-=======
-		System.out.println("karthick");
-		System.out.println("Sukumar");
-
->>>>>>> branch 'master' of https://github.com/karthick029/TestLeaf.git
-	}
+}
 
 }
